2a9122dc71ef31fc5cb44ff8a0e52e9a9b9b0216
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-11-21 17:55:28 +0300

    comments

--- src/main/java/Training/PageObject/Steps/FormSteps.java
+++ src/main/java/Training/PageObject/Steps/FormSteps.java
@@ -16,22 +16,41 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 public class FormSteps {
     public static FormFrame frame = new FormFrame();
 
+    /*
+     * Шаг : заполняется поле 'Name'
+     * ОР : Поле будет заполнено
+     */
     @Step("Заполнение поля FirstName")
     public void fillingInTheName(String name) {
         frame.formElements.firstName.val(name);
     }
 
+    /*
+     * Шаг : заполняется поле 'LastName'
+     * ОР : Поле будет заполнено
+     */
     @Step("Заполнение поля LastName")
     public void fillingInTheLastName(String lastName) {
         frame.formElements.lastName.val(lastName);
     }
 
+    /*
+     * Шаг : заполняется поле 'Email'
+     * ОР : Поле будет заполнено
+     */
     @Step("Заполнение поля Email")
     public void fillingInTheEmail(String email) {
         frame.formElements.email.val(email);
     }
 
 
+    /*
+     * Шаг : Рандомно выбираем индекс элемента из возможных
+     * Шаг : Получаем id гендеров
+     * Шаг : Добавляем в список id
+     * Шаг : Получаем id гендера по индексу
+     * ОР : Получаем строковое значение - id гендера
+     */
     private String getIdGender() {
         Random random = new Random();
         int i = random.nextInt(frame.formElements.gender.size() - 1);
@@ -43,25 +62,49 @@ public class FormSteps {
         return elements.get(i).toString();
     }
 
+    /*
+     * Шаг : в xPath подставляется id чекбокса
+     * Шаг : Клик по SelenideElement
+     * ОР : Выбран чекбокс
+     */
     @Step("Выбрать гендер")
     public void selectGender() {
         $x("//label[@for = '" + getIdGender() + "']").click();
     }
 
+    /*
+     * Шаг : Генерируется строка, состоящая из 10 цифровых символов
+     * ОР : Получаем строковое значение - номер телефона
+     */
     private String numberGeneration() {
         return RandomStringUtils.randomNumeric(10);
     }
 
+    //todo переименовать поле
+    /*
+     * Шаг : Заполняется поле 'Phone'
+     * ОР :  Поле  номер телефона заполнено
+     */
     @Step("Заполнение поля Mobile")
     public void fillInThePhoneNumber() {
         frame.formElements.phoneNumber.val(numberGeneration());
     }
 
+    //todo переименовать поле
+    /*
+     * Шаг : Клик по полю 'День рождения'
+     * ОР : Откроется календарь
+     */
     @Step("Клик по полю 'День Рождения'")
     public void clickOnTheBirthdayField() {
         frame.formElements.dateOfBirth.click();
     }
 
+    /*
+     * Шаг : Клик по "год"
+     * Шаг : выбрать 1 значение из списка
+     * ОР :В поле появится выбранная дата
+     */
     @Step("Выбрать год рождения")
     public void chooseAYearOfBirth() {
         frame.formElements.yearField.click();
@@ -72,6 +115,12 @@ public class FormSteps {
         $x("//option[@value = '" + year.stream().filter(x -> x.equals("1998")).findFirst().get() + "']").click();
     }
 
+
+    /*
+     * Шаг : Клик по "месяц"
+     * Шаг : выбрать 1 значение из списка
+     * ОР :В поле появится выбранная дата
+     */
     @Step("Выбрать месяц рождения")
     public void chooseAMonthOfBirth() {
         frame.formElements.mounthField.click();
@@ -83,7 +132,10 @@ public class FormSteps {
     }
 
 
-
+    /*
+     * Шаг : Клик по кнопке 'Submit'
+     * ОР : Появится модальное окно с указанными данными
+     */
     @Step("Клик по кнопке 'Submit'")
     public void clickButton() {
         executeJavaScript("document.getElementsByTagName(\"footer\").item(0).remove()");
