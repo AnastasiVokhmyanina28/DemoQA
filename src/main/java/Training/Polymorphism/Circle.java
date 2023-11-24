310ed7021e2c8ee4b2839e17da5c88156137380a
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-11-24 17:45:23 +0300

    ...

--- src/main/java/Training/Polymorphism/Circle.java
+++
@@ -1,9 +0,0 @@
-package Training.Polymorphism;
-
-public class Circle extends Square {
-
-    @Override
-    public void calculateTheArea(int r) {
-        System.out.println("Площадь круга = " + 3.14 * r * r);
-    }
-}
--- src/main/java/Training/Polymorphism/Rectangle.java
+++
@@ -1,8 +0,0 @@
-package Training.Polymorphism;
-
-public class Rectangle extends Square {
-    @Override
-    public void calculateTheArea(int l, int d) {
-        System.out.println("Площадь прямоугольника = " + l * d);
-    }
-}
