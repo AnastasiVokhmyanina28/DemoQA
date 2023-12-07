fe93a455c2415f674255199548a0710c4e97a447
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-12-07 09:38:02 +0300

    ....

--- src/test/java/TrainingTests/CheckFormWithAllFieldsTest.java
+++ src/test/java/TrainingTests/CheckFormWithAllFieldsTest.java
@@ -18,12 +18,27 @@ public class CheckFormWithAllFieldsTest extends BaseForm {
     @Test(description = "Проверка формы на валидацию полей", testName = "Проверка формы со всеми полями")
     public void checkFormWithAllFields() {
 
+        /**
+         * Шаг : Заполнение обязятельных полей
+         * ОР : Введенные данные отображаются в полях
+         */
         steps.checkingAFormWithMandatoryFieldsTest();
+
+        /**
+         * Шаг : Заполнение дополнительных полей
+         * ОР : Введенные данные отображаются в полях
+         */
         steps.checkingAFormAdditionalFields();
 
+        /**
+        Проверка, что выбран только 1 гендер
+         */
         assertThat(formFrame.formElements.maleGenderRadioButton.isSelected())
                 .isNotEqualTo(formFrame.formElements.femaleGenderRadioButton.isSelected());
 
+        /**
+         Проверка, что количество символов в поле равняется maxlength
+         */
         assertThat(formFrame.formElements.phoneNumber.getValue().length()).isEqualTo(Integer.valueOf(formFrame.formElements.phoneNumber.getAttribute("maxlength")));
 
         formFrame.formElements.buttonClose.shouldNotBe(Condition.visible);
